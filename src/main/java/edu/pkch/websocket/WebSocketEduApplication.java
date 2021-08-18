8403a27d819d9c81774ac9808d627d58833c20ea
pkch93 <dev.pkch@gmail.com>
2021-08-18 20:43:14 +0900

    initial commit

---
+++ src/main/java/edu/pkch/websocket/WebSocketEduApplication.java
@@ -0,0 +1,12 @@
+package edu.pkch.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketEduApplication.class, args);
+    }
+}
