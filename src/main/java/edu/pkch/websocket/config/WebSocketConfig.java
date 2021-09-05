cf1136be9da87b7444a55f2cc92e5b76031a1533
pkch93 <dev.pkch@gmail.com>
2021-09-05 13:40:55 +0900

    TIL | 210905 0140 PM | 컴파일 에러 수정

--- src/main/java/edu/pkch/websocket/config/WebSocketConfig.java
+++ src/main/java/edu/pkch/websocket/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package edu.pkch.websocket.config;
 
-import edu.pkch.websocket.handlers.EventHandlers;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -12,6 +11,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new EventHandlers(), "/event");
+
     }
 }
